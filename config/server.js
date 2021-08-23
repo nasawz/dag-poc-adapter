c6f75dbfd05c3950f9d8129c385627461f06e577
nasawz <nasa127@gmail.com>
2021-08-23 23:33:16 +0800

    fix: admin rul

--- config/server.js
+++ config/server.js
@@ -2,6 +2,7 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   admin: {
+    url: '/api',
     auth: {
       secret: env('ADMIN_JWT_SECRET', '45e4f857900f28f1e78244874d4d8a7d'),
     },
