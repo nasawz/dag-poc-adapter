6f1e0ae6f9057ceb31dceeb8a6928bc54301496d
nasawz <nasa127@gmail.com>
2021-09-02 22:02:46 +0800

    feat: constrains

--- config/functions/bootstrap.js
+++ config/functions/bootstrap.js
@@ -11,57 +11,57 @@
  */
 
 module.exports = async () => {
-  const { Client } = require("hazelcast-client");
-  const hz = await Client.newHazelcastClient({
-    clusterName: "domain",
-    network: {
-      clusterMembers: ["192.168.5.177"],
-    },
-  });
-  var io = require("socket.io")(strapi.server, {
-    path: "/ws/",
-  });
-  // var io = require('socket.io')(strapi.server, {
-  //     cors: {
-  //       origin: "http://localhost:8080",
-  //       methods: ["GET", "POST"],
-  //       allowedHeaders: ["my-custom-header"],
-  //       credentials: true
-  //     }
+  // const { Client } = require("hazelcast-client");
+  // const hz = await Client.newHazelcastClient({
+  //   clusterName: "domain",
+  //   network: {
+  //     clusterMembers: ["192.168.5.177"],
+  //   },
   // });
+  // var io = require("socket.io")(strapi.server, {
+  //   path: "/ws/",
+  // });
+  // // var io = require('socket.io')(strapi.server, {
+  // //     cors: {
+  // //       origin: "http://localhost:8080",
+  // //       methods: ["GET", "POST"],
+  // //       allowedHeaders: ["my-custom-header"],
+  // //       credentials: true
+  // //     }
+  // // });
 
-  io.on("connection", function (socket) {
-    socket.on("join", async ({ experimentId }, callback) => {
-      console.log("experimentId", experimentId);
-      callback();
-    });
+  // io.on("connection", function (socket) {
+  //   socket.on("join", async ({ experimentId }, callback) => {
+  //     console.log("experimentId", experimentId);
+  //     callback();
+  //   });
 
-    socket.on("queryJob", async ({ experimentId, nodeIds }, callback) => {
-      const map = await hz.getMap(`${experimentId}`);
-      let v = await map.getAll(nodeIds);
-      console.log(v);
-      callback(null, { data: v });
-    });
+  //   socket.on("queryJob", async ({ experimentId, nodeIds }, callback) => {
+  //     const map = await hz.getMap(`${experimentId}`);
+  //     let v = await map.getAll(nodeIds);
+  //     console.log(v);
+  //     callback(null, { data: v });
+  //   });
 
-    socket.on("updateNode", async ({ data }, callback) => {
-      try {
-        for (let key in data) {
-          console.log(`${key} : ${data[key]}`);
-          const map = await hz.getMap(key);
-          await map.put(`${key}`, data[key])
-        }
-        callback(null, { data: "success" });
-      } catch (error) {
-        callback(error);
-      }
-    });
+  //   socket.on("updateNode", async ({ data }, callback) => {
+  //     try {
+  //       for (let key in data) {
+  //         console.log(`${key} : ${data[key]}`);
+  //         const map = await hz.getMap(key);
+  //         await map.put(`${key}`, data[key])
+  //       }
+  //       callback(null, { data: "success" });
+  //     } catch (error) {
+  //       callback(error);
+  //     }
+  //   });
 
-    // console.log("=========connection");
-    // socket.on("join", ({ username, room }) => {
-    //   console.log("user connected");
-    //   console.log("username is ", username);
-    //   console.log("room is...", room);
-    // });
-  });
+  //   // console.log("=========connection");
+  //   // socket.on("join", ({ username, room }) => {
+  //   //   console.log("user connected");
+  //   //   console.log("username is ", username);
+  //   //   console.log("room is...", room);
+  //   // });
+  // });
 };
 // https://webpack.js.org/configuration/dev-server/#websocketurl
